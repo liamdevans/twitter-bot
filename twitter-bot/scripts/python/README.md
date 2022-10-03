006a6ccdd4d7297aab48edb9953a77d63026c9cd
liamdevans <liamdevans@yahoo.co.uk>
2022-10-03 14:19:37 +0100

    Add delete_all.py tweets functionality

---
+++ twitter-bot/scripts/python/README.md
@@ -0,0 +1,9 @@
+## `delete_all.py`
+To delete all tweets from an account,
+1. Run the `delete_all.py` script.
+2. Click on the link supplied in the terminal.
+   1. Select 'Accept'
+   2. In the URL of the subsequent page, copy the `oauth_verifier` (e.g. cX1ZFJrjdpqlbXlI7v2yI5MqFVUd4trD) and paste into the terminal.
+   3. Type 'yes' to confirm.
+
+All tweets should then be deleted from your account.
---
+++ twitter-bot/scripts/python/delete_all.py
@@ -0,0 +1,44 @@
+# deletes all tweets from account
+import traceback
+
+import tweepy
+
+from configs import keys
+
+
+def oauth_login():
+    """Authenticate with twitter using OAuth"""
+    consumer_key = keys.CONSUMER_API_KEY
+    consumer_secret = keys.CONSUMER_API_KEY_SECRET
+
+    auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret)
+    auth_url = auth.get_authorization_url()
+
+    verify_code = input(
+        "Authenticate at %s and then enter you verification code here > " % auth_url
+    )
+    auth.get_access_token(verify_code)
+
+    return tweepy.API(auth)
+
+
+def batch_delete(api):
+    print(
+        "You are about to delete all tweets from the account @%s."
+        % api.verify_credentials().screen_name
+    )
+    print("Does this sound ok? There is no undo! Type yes to carry out this action.")
+    do_delete = input("> ")
+    if do_delete.lower() == "yes":
+        for status in tweepy.Cursor(api.user_timeline).items():
+            try:
+                api.destroy_status(status.id)
+                print("Deleted:", status.id)
+            except Exception:
+                traceback.print_exc()
+                print("Failed to delete:", status.id)
+
+
+if __name__ == '__main__':
+    api = oauth_login()
+    batch_delete(api)
