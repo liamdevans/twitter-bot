42d1d3440afd7526d120e10e396838f4ccf58336
liamdevans <liamdevans@yahoo.co.uk>
2022-10-07 09:50:03 +0100

    Add test for make_date_readable

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,14 +1,41 @@
-from twitter_bot.helpers import format_tweet
+import datetime
+
+from twitter_bot.helpers import format_tweet, make_date_readable
 import pytest
 
-tweet_data = [
+dummy_tweet_data = [
     ("First tweet", "First tweet\n\n#twitterclarets"),
     ("", "\n\n#twitterclarets"),
 ]
 
 
+dummy_date_data = [
+    (
+        datetime.datetime.strptime("2022-01-01 20:00", "%Y-%m-%d %H:%M"),
+        "Sat 1st Jan at 8:00 PM",
+    ),
+    (
+        datetime.datetime.strptime("2022-01-02 08:00", "%Y-%m-%d %H:%M"),
+        "Sun 2nd Jan at 8:00 AM",
+    ),
+    (
+        datetime.datetime.strptime("2022-01-03 12:00", "%Y-%m-%d %H:%M"),
+        "Mon 3rd Jan at 12:00 PM",
+    ),
+    (
+        datetime.datetime.strptime("2022-01-04 00:00", "%Y-%m-%d %H:%M"),
+        "Tue 4th Jan at 12:00 AM",
+    ),
+]
+
+
 @pytest.mark.parametrize(
-    "tweet, expected", tweet_data, ids=["Standard Tweet", "Tweet with no text"]
+    "tweet, expected", dummy_tweet_data, ids=["Standard Tweet", "Tweet with no text"]
 )
 def test_format_tweet(tweet, expected):
     assert format_tweet(tweet) == expected
+
+
+@pytest.mark.parametrize("datetime_object, expected", dummy_date_data)
+def test_make_date_readable(datetime_object, expected):
+    assert make_date_readable(datetime_object) == expected
