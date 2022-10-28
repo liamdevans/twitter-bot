0e01bff684b81656c0a716e907875127160dbe9c
liamdevans <liamdevans@yahoo.co.uk>
2022-10-28 16:03:55 +0100

    Parameterize cron schedule

--- configs/fbref.py
+++ configs/fbref.py
@@ -5,3 +5,5 @@ Contains useful configurations relating to the stats website www.fbref.com
 championship_url = r"https://fbref.com/en/comps/10/Championship-Stats"
 
 burnley_url = r"https://fbref.com/en/squads/943e8050/Burnley-Stats"
+
+cron_schedule = "0 13 * * *"
